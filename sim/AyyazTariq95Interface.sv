ffeee0752127bf35e7248c7b25b4223d85686737
AyyazTariq95 <102603570+AyyazTariq95@users.noreply.github.com>
2022-04-21 22:21:46 +0500

    Update AyyazTariq95Interface.sv

--- sim/AyyazTariq95Interface.sv
+++ sim/AyyazTariq95Interface.sv
@@ -1,16 +1,60 @@
-interface my_interface(input bit HCLK, HRESETn);
-logic HWRITE, HREADY, HREADYOUT, HRESP;
+interface my_interface(input logic HCLK, HRESETn);
+logic HWRITE, HREADY, HREADYOUT, HRESP, HMASTLOCK;
 logic[31:0] HADDR, HWDATA, HRDATA;
 logic[2:0] HSIZE, HBURST;
 logic[3:0] HPROT;
 logic[1:0] HSEL,HTRANS;
 
-modport DUT(input HCLK, HRESETn, HSEL, HADDR, HWDATA, HWRITE, HSIZE, HBURST, HPROT, HTRANS, HREADY,
-            output HRDATA, HREADYOUT, HRESP);
+modport dut(
+      	input  		HRESETn,
+      	input  		HCLK,
+      	input   	HSEL,
+  	input   	HADDR,
+ 	input   	HWDATA,
+  	output 		HRDATA,
+  	input           HWRITE,
+  	input       	HSIZE,
+  	input       	HBURST,
+  	input       	HPROT,
+  	input       	HTRANS,
+        input           HMASTLOCK,
+  	output 		HREADYOUT,
+  	input           HREADY,
+  	output          HRESP
+  );
+ modport driver(
+      	input  	        HRESETn,
+      	input  	        HCLK,
+      	output   	HSEL,
+  	output   	HADDR,
+ 	output   	HWDATA,
+  	output          HWRITE,
+  	output       	HSIZE,
+  	output       	HBURST,
+  	output       	HPROT,
+  	output       	HTRANS,
+  	input           HREADY,
+        output          HMASTLOCK,
+	input           HRDATA,
+	input           HRESP
+  );
 
-modport TEST(input HCLK, HRESETn, HRDATA, HREADYOUT, HRESP,
-             output HSEL, HADDR, HRDATA, HWRITE, HSIZE, HBURST, HPROT, HTRANS, HREADY);
-
-modport MON(input HCLK, HRDATA, HREADYOUT, HRESP);
+ modport monitor(
+	input  		HRESETn,
+      	input  		HCLK,
+      	input   	HSEL,
+  	input   	HADDR,
+ 	input   	HWDATA,
+  	input           HWRITE,
+  	input       	HSIZE,
+  	input       	HBURST,
+  	input       	HPROT,
+  	input       	HTRANS,
+        input           HMASTLOCK,
+  	input           HREADY,	
+	input 		HRDATA,
+	input 		HREADYOUT,
+	input          HRESP);
 
 endinterface
+
