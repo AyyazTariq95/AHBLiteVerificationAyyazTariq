ac3a7d950f7ed27f94f214d51fae4b2ae30012e7
AyyazTariq95 <102603570+AyyazTariq95@users.noreply.github.com>
2022-04-21 23:16:04 +0500

    Add files via upload

---
+++ sim/AyyazTariq95Test1.sv
@@ -0,0 +1,16 @@
+//reset testing
+`include "MP2Environment.sv"
+program Test(my_interface my_vif);
+  Environment Env;
+  initial begin
+    Env = new(my_vif);
+    Env.Driv.rest;
+    Env.Gen.end_addr = 256;
+    Env.Gen.current_add = 4;
+    Env.Gen.current_data = 44;
+    Env.Gen.rand_me= 1;
+    my_vif.HWRITE <= 1;
+    my_vif.HTRANS <= 0;
+    Env.run_env(Env.Gen.current_add,Env.Gen.end_addr,Env.Gen.current_data,Env.Gen.rand_me);
+  end
+endprogram
---
+++ sim/AyyazTariq95Test2.sv
@@ -0,0 +1,14 @@
+`include "MP2Environment.sv"
+program Test1(my_interface my_vif);
+  Environment Env;
+initial begin
+    Env = new(my_vif);
+    Env.Gen.rand_me = 0;
+    Env.Gen.current_add = 5;
+    Env.Gen.end_addr = 256;
+    Env.Gen.current_data = trr.HWDATA;
+    my_vif.HWRITE <=1;
+    my_vif.HTRANS <=0;
+    Env.run_env(Env.Gen.current_add,Env.Gen.end_addr,Env.Gen.current_data,Env.Gen.rand_me);
+  end
+endprogram
