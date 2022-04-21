31671e99d6f96d10f6134b5d7e938406eb39e715
AyyazTariq95 <102603570+AyyazTariq95@users.noreply.github.com>
2022-04-21 23:01:23 +0500

    Update AyyazTariq95_tb.sv

--- sim/AyyazTariq95_tb.sv
+++ sim/AyyazTariq95_tb.sv
@@ -1,29 +1,16 @@
 import ahb3lite_pkg::*;
-`include "MP2Interface.sv"
-`include "MP2TestProg.sv"
-module top_tb;
+`include "AyyazTariq95Interface.sv"
+`include "AyyazTariq95TestProg.sv"
+module AyyazTariq95_tb;
   bit clk;
   bit reset;
   //reg curr_addr;
   //reg numofbeats;
-  
   //clock generation
   always #5 clk = ~clk;
-  
-  //reset Generation
-  //initial begin
-  //  reset = 1;
-  //  #5 reset =0;
-  //end
-  
-  
-  //creatinng instance of interface, inorder to connect DUT and testcase
   my_interface my_intf(clk,reset);
   //my_intf.HADDR <= 32'hcafe_dada;
-  //Testcase instance, interface handle is passed to test as an argument
   Test t1(my_intf);
-  
-  //DUT instance, interface signals are connected to the DUT ports
    ahb3lite_sram1rw DUT(	
 				.HRESETn (my_intf.HRESETn),
 				.HCLK	(my_intf.HCLK),
@@ -40,14 +27,4 @@ module top_tb;
 				.HREADY	(my_intf.HREADY),
 				.HRESP	(my_intf.HRESP)	
                                );
-
-//initial begin
-//numofbeats = 6;
-//curr_addr = 10;
-//my_intf.HWRITE = 1'b1;
-//my_intf.HADDR = 32'hcafe_dada;
-//my_intf.HWDATA = 32'h1234_5678;
-//#2
-//my_intf.HWRITE = 1'b0;
-//end
 endmodule
